296998eab346101bac748709e7cd1d64aa5e3551
hamez gjonaj <92572297+Hamezgjonaj@users.noreply.github.com>
2023-02-22 22:19:45 +0100

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,38 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { QuoteBoxComponent } from "./quote-box/quote-box.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, QuoteBoxComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'random-quote-machine'`, () => {
+  it("should render loading text when loading", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('random-quote-machine');
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(app.loading).toBeTrue();
+    expect(compiled.querySelector("#loading").textContent).toEqual(
+      "loading..."
+    );
   });
 
-  it('should render title', () => {
+  it("should render QuoteBoxComponent after loading", async () => {
     const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    await app.fetchData();
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('random-quote-machine app is running!');
+    const compiled = fixture.nativeElement;
+    expect(app.loading).toBeFalse();
+    expect(compiled.querySelector("app-root app-quote-box")).toBeDefined();
   });
 });
